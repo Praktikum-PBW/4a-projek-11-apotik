c6d053ba8c01251ac8f258ef3d3d96ac5c16bc2e
AsyifaMaulana1412 <74490650+yourLogic01@users.noreply.github.com>
2022-04-29 19:58:13 +0700

    update pertemuan 11

---
+++ user/menu.php
@@ -0,0 +1,23 @@
+<?php
+if(isset($_GET['page'])){
+    include '../lib/koneksi.php';
+    $page = $_GET['page'];
+
+    switch($page){
+        case 'home':
+            include 'home-user.php';
+            break;
+        case 'profile':
+            include 'profile.php';
+            break;
+        case 'obat':
+            include 'obat.php';
+            break;
+        default:
+            echo"halaman tidak ditemukan";
+            break;
+    }
+}else{
+    include 'home-user.php';
+}
+?>
